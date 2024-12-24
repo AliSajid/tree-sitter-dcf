54258b8d56d4840294b355ef035573f201f7c6ac
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-12-23 21:26:37 -0500

    feat: add the primary grammar
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- grammar.js
+++ grammar.js
@@ -1,3 +1,10 @@
+// SPDX-FileCopyrightText: 2024 Ali Sajid Imami
+//
+// SPDX-License-Identifier: MIT
+
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-undef */
 /**
  * @file Tree-sitter parser for Debian Control Format files
  * @license MIT
@@ -7,59 +14,60 @@
 // @ts-check
 
 module.exports = grammar({
-  name: 'dcf',
+    name: 'dcf',
 
-  extras: $ => [
-    /[ \t]/, // Allow spaces and tabs as extras
-    $.comment // Comments are treated as extras
-  ],
+    extras: _$ => [
+        /[ \t]/, // Allow spaces and tabs as extras
+        _$.comment // Comments are treated as extras
+    ],
 
-  rules: {
-    source_file: $ => seq(
-      optional(/\r?\n/), // Allow optional leading newline
-      sepBy($.stanza_separator, $.stanza), // Stanzas separated by stanza_separator
-      optional($.stanza_separator) // Allow optional trailing stanza separator
-    ),
+    rules: {
+        source_file: _$ =>
+            seq(
+                optional(/\r?\n/), // Allow optional leading newline
+                sepBy(_$.stanza_separator, _$.stanza), // Stanzas separated by stanza_separator
+                optional(_$.stanza_separator) // Allow optional trailing stanza separator
+            ),
 
-    stanza: $ => repeat1($.field), // A stanza consists of one or more fields
+        stanza: _$ => repeat1(_$.field), // A stanza consists of one or more fields
 
-    field: $ => seq(
-      $.field_name,
-      ':',
-      optional($.field_value), // Field values are optional
-      /\r?\n/ // Field ends at the newline
-    ),
+        field: _$ =>
+            seq(
+                _$.field_name,
+                ':',
+                optional(_$.field_value), // Field values are optional
+                /\r?\n/ // Field ends at the newline
+            ),
 
-    field_name: $ =>
-      token(seq(
-        /[A-Za-z0-9!-/;-~]/, // Characters allowed in field names
-        repeat(/[A-Za-z0-9!-/;-~]/) // One or more characters
-      )),
+        field_name: _$ =>
+            token(
+                seq(
+                    /[A-Za-z0-9!-/;-~]/, // Characters allowed in field names
+                    repeat(/[A-Za-z0-9!-/;-~]/) // One or more characters
+                )
+            ),
 
-    field_value: $ => choice(
-      $.simple_value, // A single-line value
-      $.folded_value  // A folded, multiline value
-    ),
+        field_value: __$ =>
+            choice(
+                __$.simple_value, // A single-line value
+                __$.folded_value // A folded, multiline value
+            ),
 
-    simple_value: $ => token(/[^\r\n]+/), // Stop parsing immediately
+        simple_value: _$ => token(/[^\r\n]+/), // Stop parsing immediately
 
-    folded_value: $ => seq(
-      /[^\r\n]+/, // First line of the folded value
-      repeat1(seq(/\r?\n[ \t]+/, /[^\r\n]+/)) // Continuation lines start with space/tab
-    ),
+        folded_value: _$ =>
+            seq(
+                /[^\r\n]+/, // First line of the folded value
+                repeat1(seq(/\r?\n[ \t]+/, /[^\r\n]+/)) // Continuation lines start with space/tab
+            ),
 
-    comment: $ => token(seq('#', /[^\r\n]*/)), // Comment lines start with #
+        comment: _$ => token(seq('#', /[^\r\n]*/)), // Comment lines start with #
 
-    stanza_separator: $ => /\r?\n(\r?\n|[ \t]*\r?\n)/ // Empty or whitespace-only lines
-  },
-
-  //conflicts: $ => [
-  //  [$.simple_value, $.folded_value]
-  //]
-});
+        stanza_separator: _$ => /\r?\n(\r?\n|[ \t]*\r?\n)/ // Empty or whitespace-only lines
+    }
+})
 
 // Utility function to separate rules by a delimiter
 function sepBy(delimiter, rule) {
-  return seq(rule, repeat(seq(delimiter, rule)));
+    return seq(rule, repeat(seq(delimiter, rule)))
 }
-
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,3 +1,7 @@
+;;; SPDX-FileCopyrightText: 2024 Ali Sajid Imami
+;;;
+;;; SPDX-License-Identifier: MIT
+
 ; Basic syntax highlighting only
 (field_name) @field
 (simple_value) @string
