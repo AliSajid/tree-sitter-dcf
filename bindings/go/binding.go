54348f36258c5dfa81527c93ef59e01c35d1c87a
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-12-23 20:40:47 -0500

    feat: add go bindings
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- bindings/go/binding.go
+++ bindings/go/binding.go
@@ -9,5 +9,5 @@ import "unsafe"
 
 // Get the tree-sitter Language for this grammar.
 func Language() unsafe.Pointer {
-	return unsafe.Pointer(C.tree_sitter_dcf())
+    return unsafe.Pointer(C.tree_sitter_dcf())
 }
--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -13,3 +13,26 @@ func TestCanLoadGrammar(t *testing.T) {
 		t.Errorf("Error loading DCF grammar")
 	}
 }
+
+func TestDCFGrammar(t *testing.T) {
+	parser := e_sitter.NewParser()
+	parser.SetLanguage(e_sitter_dcf.GetLanguage())
+
+	tests := []struct {
+		name   string
+		source string
+	}{
+		{"Simple Field", "Package: mypackage\n"},
+		{"Folded Field", "Description: A test package\n A folded line\n"},
+		{"Multiple Stanzas", "Package: one\n\nPackage: two\n"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			tree := parser.Parse([]byte(test.source))
+			if tree == nil {
+				t.Errorf("Failed to parse: %s", test.source)
+			}
+		})
+	}
+}
